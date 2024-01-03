6f5c50865ac6ba0b881f68f89b7925fd9f8bc847
MAT-2 <mpcasey30@gmail.com>
2024-01-03 14:10:40 -0600

    Added basic search bar.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton, Icon } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    // Search Bar
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      {/* Input Field in SearchBar */}
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value=""
+        onChange={() => {}}
+      />
+      {/* Search Icon in SearchBar input field */}
+      <IconButton type="submit" sx={{ p: "10px", color: "green" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
 };
 
 export default SearchBar;
